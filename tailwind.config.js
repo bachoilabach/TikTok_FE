b0ef008f27f67a232af4a73ba91f54cc8f93b3ba
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-08-25 18:59:09 +0700

    add: color and height

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = withMT({
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		color: {
+			main: '#121212',
+			hover: '#444444',
+		},
+		height: {
+			'screen-minus-59': 'calc(100vh - 59px)',
+		},
 	},
 	plugins: [],
 });
