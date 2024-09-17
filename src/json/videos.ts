4d6c1e75ffd84e5ac933d301f07373e217e47ca7
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-09-17 14:20:07 +0700

    add: static video (fake api)

---
+++ src/json/videos.ts
@@ -0,0 +1,40 @@
+import { VideoProps } from '../components/Video';
+
+export const videos: VideoProps[] = [
+	{
+		videoID: '1',
+		userID: 'user_01',
+		title:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		videoUrl: 'src/video/khabank.mp4',
+		duration: 120,
+		viewQuantity: 1000,
+		commentQuantity: 100,
+		likeQuantity: 200,
+		// createdDate: new Date('2024-09-01'),
+	},
+	{
+		videoID: '2',
+		userID: 'user_02',
+		title:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		videoUrl: 'src/video/domixi.mp4',
+		duration: 120,
+		viewQuantity: 1000,
+		commentQuantity: 100,
+		likeQuantity: 200,
+		// createdDate: new Date('2024-09-01'),
+	},
+	// {
+	// 	videoID: '3',
+	// 	userID: 'user_03',
+	// 	title:
+	// 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur. Excepteur sint occaecat cupidatat non proident,sunt in culpa qui officia deserunt mollit anim id est laborum.',
+	// 	videoUrl: 'src/video/test.mp4',
+	// 	duration: 120,
+	// 	viewQuantity: 1000,
+	// 	commentQuantity: 100,
+	// 	likeQuantity: 200,
+	// 	// createdDate: new Date('2024-09-01'),
+	// },
+];
