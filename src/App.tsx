1d9a562841d08ff3c004a0b439a7d81166d66308
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-10-08 22:54:43 +0700

    add: upload route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
-import ForYou from "./pages/ForYou";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import ForYou from './pages/ForYou';
+import UploadLayout from './layout/UploadLayout';
+import UploadPage from './pages/UploadPage';
 
 export default function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout/>}>
-            <Route index element={<ForYou/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<MainLayout />}>
+						<Route index element={<ForYou />} />
+					</Route>
+					<Route path="/upload" element={<UploadLayout />}>
+						<Route index element={<UploadPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
