f49e84688f2f58a83d4169cf96c8c5562fdacf40
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-10-20 17:27:38 +0700

    chore: do not use strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css';
 import { UserProvider } from './context/UserContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
+	// <StrictMode>
 		<UserProvider>
 			<App />
 		</UserProvider>
-	</StrictMode>
+	// </StrictMode>
 );
