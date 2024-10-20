fcd5631ae0458c2de92db7168dbb9a8c57dd33c4
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-10-20 17:28:37 +0700

    add: upload video to cloud func

--- src/api/video.ts
+++ src/api/video.ts
@@ -1,12 +1,39 @@
-import axiosInstance from '../utils/axios';
+import axiosInstance, { socket } from '../utils/axios';
 
 const handleGetAllVideoApi = async () => {
 	try {
-		const response = await axiosInstance.get('/api/video');
+		const response = await axiosInstance.get('/api/video/get-all-video');
 		return response.data;
 	} catch (error) {
 		console.log(error);
 	}
 };
 
-export { handleGetAllVideoApi };
+const handleUploadVideoToCloudApi = async (video: File) => {
+	try {
+		const formData = new FormData();
+		formData.append('video', video);
+		const socketId = socket.id;
+		if (socketId) {
+			formData.append('socketId', socketId); // Thêm socketId nếu tồn tại
+		} else {
+			throw new Error(
+				'Socket ID is undefined. Unable to track upload progress.'
+			);
+		}
+
+		// Gửi video lên server
+		const response = await axiosInstance.post('/api/video/upload', formData, {
+			headers: {
+				'Content-Type': 'multipart/form-data',
+			},
+		});
+
+		return response.data;
+	} catch (error) {
+		console.error('Error uploading video:', error);
+		throw error;
+	}
+};
+
+export { handleGetAllVideoApi, handleUploadVideoToCloudApi };
